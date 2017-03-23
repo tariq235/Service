90d845d0813ce5587a7a7039ed9fb3368b409132
Tariq Mohammad <tariq@Tariqs-MacBook-Pro.local>
2017-03-23 12:27:41 +0800

    Commit

---
+++ Service/Service/Service.h
@@ -0,0 +1,19 @@
+//
+//  Service.h
+//  Service
+//
+//  Created by Tariq Mohammad on 3/23/17.
+//  Copyright © 2017 Tariq Mohammad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Service.
+FOUNDATION_EXPORT double ServiceVersionNumber;
+
+//! Project version string for Service.
+FOUNDATION_EXPORT const unsigned char ServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Service/PublicHeader.h>
+
+
---
+++ Service/Service/ServiceLayer.swift
@@ -0,0 +1,9 @@
+//
+//  ServiceLayer.swift
+//  Service
+//
+//  Created by Tariq Mohammad on 3/23/17.
+//  Copyright © 2017 Tariq Mohammad. All rights reserved.
+//
+
+import Foundation
